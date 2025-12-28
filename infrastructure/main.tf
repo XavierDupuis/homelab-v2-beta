c8837868d662ac925c2a2e71a699799f3b6aa6e4
XavierDupuis <github-xavierdupuis@outlook.com>
2025-12-28 17:41:03 -0500

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,17 +1,20 @@
 module "mission_critical_vm" {
   source         = "./modules/vm"
+  PROXMOX_NODE_NAME = var.PROXMOX_NODE_NAME
   VM_NAME        = "MissionCritical"
   VM_ID          = 8101
 }
 
 module "data_critical_vm" {
   source         = "./modules/vm"
+  PROXMOX_NODE_NAME = var.PROXMOX_NODE_NAME
   VM_NAME        = "DataCritical"
   VM_ID          = 8102
 }
  
 module "common_vm" {
   source         = "./modules/vm"
+  PROXMOX_NODE_NAME = var.PROXMOX_NODE_NAME
   VM_NAME        = "Common"
   VM_ID          = 8103
 }
