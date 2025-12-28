554ce2b6de9f9e6fb00c67e4f64bec18b1ad258f
XavierDupuis <github-xavierdupuis@outlook.com>
2025-12-28 17:33:50 -0500

    move to main

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -9,10 +9,3 @@ terraform {
     }
   }
 }
-
-provider "proxmox" {
-  pm_api_url          = var.PROXMOX_URL
-  pm_api_token_id     = var.PROXMOX_TOKEN_ID
-  pm_api_token_secret = var.PROXMOX_TOKEN_SECRET
-  pm_tls_insecure     = true
-}
