9b6c0df1ae57d34049a274e908cd6f4b3183e34b
XavierDupuis <github-xavierdupuis@outlook.com>
2025-12-28 17:39:12 -0500

    Update providers.tf

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,3 +1,15 @@
+# https://registry.terraform.io/providers/Telmate/proxmox/latest/docs
+# https://github.com/Telmate/terraform-provider-proxmox
+terraform {
+  required_version = ">= 0.13.0"
+  required_providers {
+    proxmox = {
+      source = "telmate/proxmox"
+      version = "3.0.2-rc04" # Needed for PVE 9
+    }
+  }
+}
+
 provider "proxmox" {
   pm_api_url          = var.PROXMOX_URL
   pm_api_token_id     = var.PROXMOX_TOKEN_ID
