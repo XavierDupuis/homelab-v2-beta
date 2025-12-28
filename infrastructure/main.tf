372ced3c00aa31ebaede683fadf5fdb09fae866e
XavierDupuis <github-xavierdupuis@outlook.com>
2025-12-28 17:38:52 -0500

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -16,9 +16,3 @@ module "common_vm" {
   VM_ID          = 8103
 }
 
-provider "proxmox" {
-  pm_api_url          = var.PROXMOX_URL
-  pm_api_token_id     = var.PROXMOX_TOKEN_ID
-  pm_api_token_secret = var.PROXMOX_TOKEN_SECRET
-  pm_tls_insecure     = true
-}
