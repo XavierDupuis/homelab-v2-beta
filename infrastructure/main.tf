863fbd58028208dc1829ad07dec31da42360b86e
XavierDupuis <github-xavierdupuis@outlook.com>
2026-01-03 11:55:46 -0500

    wip

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,19 +1,19 @@
 module "mission_critical_vm" {
-  source         = "./modules/vm"
+  source         = "./modules/cloned-vm"
   PROXMOX_NODE_NAME = var.PROXMOX_NODE_NAME
   VM_NAME        = "MissionCritical"
   VM_ID          = 8101
 }
 
 # module "data_critical_vm" {
-#   source         = "./modules/vm"
+#   source         = "./modules/cloned-vm"
 #   PROXMOX_NODE_NAME = var.PROXMOX_NODE_NAME
 #   VM_NAME        = "DataCritical"
 #   VM_ID          = 8102
 # }
  
 # module "common_vm" {
-#   source         = "./modules/vm"
+#   source         = "./modules/cloned-vm"
 #   PROXMOX_NODE_NAME = var.PROXMOX_NODE_NAME
 #   VM_NAME        = "Common"
 #   VM_ID          = 8103
