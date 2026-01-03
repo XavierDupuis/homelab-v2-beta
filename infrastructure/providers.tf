3ad5ce38bda1186f2c92652f0b6fb18484445524
XavierDupuis <github-xavierdupuis@outlook.com>
2026-01-03 13:17:31 -0500

    Update providers.tf

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -14,8 +14,8 @@ provider "proxmox" {
   endpoint            = var.PROXMOX_ENDPOINT
   # api_token           = "${var.PROXMOX_USERNAME}!${var.PROXMOX_TOKEN_ID}=${var.PROXMOX_TOKEN_SECRET}"
   insecure            = true
-  username = var.PROXMOX_USERNAME
-  password = var.PROXMOX_PASSWORD
+  # username = var.PROXMOX_USERNAME
+  # password = var.PROXMOX_PASSWORD
 
   # ssh {
   #   # agent = true
